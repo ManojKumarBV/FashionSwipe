d86c626baa23774d0fc8e2883498aea9a06161cf
B V Manoj Kumar Reddy <manoj@Manus-MBP.local>
2025-05-12 09:39:19 +0530

    updated version tag

--- src/components/Version.tsx
+++ src/components/Version.tsx
@@ -1,25 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 import config from "../../capacitor.config";
+
 const Version = () => {
-  const [version, setVersion] = useState('');
-  const [deployEnv, setDeployEnv] = useState('');
+  const deployEnv = useMemo(() => {
+    const serverUrl = config?.server?.url;
+    return serverUrl?.startsWith('https') ? 'Prod' : 'Dev';
+  }, [config?.server?.url]);
 
-  useEffect(() => {
-
-    const serverConfig = config.server;
-    const deployEnv = serverConfig?.url?.startsWith('https') ? 'Prod' : 'Dev';
-    setDeployEnv(deployEnv);
-
-    fetch('/package.json')
-      .then(response => response.json())
-      .then(data => {
-        setVersion(data.version);
-      })
-      .catch(error => {
-        console.error('Error fetching version:', error);
-        setVersion('Version not available');
-      });
-  }, []);
+  const version = import.meta.env.VITE_VERSION;
 
   return (
     <div className="fixed bottom-2 right-1/2 translate-x-1/2 text-xs text-gray-500 px-2 py-1 sm:right-2 sm:translate-x-0">
